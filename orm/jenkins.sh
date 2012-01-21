eb8e611324efdafed1557570f1ba6cf6e9a0b142
Cvacubo <cvacubo@gmail.com>
2012-01-21 20:50:51 +0400

    Jenkins script

---
+++ orm/jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create virtual environment
+/usr/bin/virtualenv --no-site-packages orm_env
+. orm_env/bin/activate
+
+# Install fabric package
+pip install fabric
+
+# Deploy requirements packages
+fab install_requirements -H localhost
+
+# Run unittests and coverage them
+coverage erase
+coverage run orm/tests/run.py --with-xunit
+coverage xml
+
+# Pep8 and Pylint violations
+pep8 --repeat --ignore=E501,W391 orm | perl -ple 's/: ([WE]\d+)/: [$1]/' > reports/pylint.report
+pylint --rcfile pylint.rc orm/*.py >> reports/pylint.report
